5ff20aa2069b7bde0134352820cad12266f83f15
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-12 18:06:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+
+
+
+let dodger = document.getElementById("dodger");
+
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(evt) {
+    if (evt.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(evt) {
+    if (evt.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
